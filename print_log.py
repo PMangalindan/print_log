fb6e48adedc7d5754d6cd0db05df0505e5c57065
PMangalindan <118822573+PMangalindan@users.noreply.github.com>
2023-06-06 12:16:34 +0800

    Add files via upload

---
+++ print_log.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+def plog(string):
+    """prints and logs string"""
+    global session_id
+    string = str(string)
+    
+    
+    print(string)
+    
+    
+    
+    try:
+        
+        session_id = session_id
+        
+    except:
+        from datetime import datetime
+        date_now = datetime.now().strftime("%m-%d-%y_%H%M%S") 
+        session_id = str(date_now)
+        
+        
+    output_folder = r'print_log\\'    
+    
+    import os
+    if os.path.exists(output_folder): 
+        pass
+    else:    
+        
+        os.mkdir(output_folder)
+        
+    with open(f'{output_folder}/plog_{session_id}.txt', 'a') as f:
+        f.write(string)
+        f.write('\n')
+    
+    return string
+
